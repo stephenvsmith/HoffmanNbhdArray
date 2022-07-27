3b29e38826cbb72f30cad41cb01a67e487713ad7
stephenvsmith <stephensmith13424@gmail.com>
2022-07-26 22:50:36 -0700

    Some folder name adjustments

--- initializetest.R
+++ initializetest.R
@@ -3,7 +3,7 @@
 home_dir <- '/Users/stephensmith/'
 scratch_dir <- home_dir
 data_gen_file <- paste0(home_dir,'Dropbox/Academics/Research/Code/Scripts/data_gen.R')
-result_dir <- paste0(home_dir,'Desktop/','ResultsNbhd-',format(Sys.Date(),"%m-%y"))
+result_dir <- paste0(home_dir,'Desktop/','ResultsNbhdArray-',format(Sys.Date(),"%m-%y"))
 rds_dir <- paste0(home_dir,'Dropbox/Academics/Research/Code/Networks/rds')
 source(paste0(home_dir,'Dropbox/Academics/Research/Code/Scripts/HoffmanNbhdArray/helperfunctions.R'))
 
